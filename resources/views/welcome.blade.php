6097426e5cef3af6f4157eb5d1beab6b1fec35db
Raman Verma <devramanverma@gmail.com>
2023-07-18 17:10:14 +0530

    Prepared views for react

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>UyScuti</title>
+        
+        @viteReactRefresh
+        @vite('resources/js/index.jsx')
+    </head>
+    <body class="antialiased">
+      <div id="root"></div>
+    </body>
+</html>

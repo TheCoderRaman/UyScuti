22fea1ee5e1c9f4c82e3206209ecbf71fa54911c
Raman Verma <devramanverma@gmail.com>
2023-07-20 00:10:44 +0530

    Added jwt auth controller routes

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::group([
+    'middleware' => 'api','prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});

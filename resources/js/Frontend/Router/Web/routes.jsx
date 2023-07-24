79b27390e3d6eccc70aac4b40b8b61f6754500b2
Raman Verma <devramanverma@gmail.com>
2023-07-25 00:19:07 +0530

    Added routes registrar boiler plate

---
+++ resources/js/Frontend/Router/Web/routes.jsx
@@ -0,0 +1,10 @@
+/**
+ * This module give us a single location for altering any
+ * route in the entire application at any point of 
+ * time in the future.
+ * 
+ * @var const Object routes
+ */
+export const routes = {
+    //
+};

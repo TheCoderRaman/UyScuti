b880a533e469785a576680bbac932878f292ecb9
Raman Verma <devramanverma@gmail.com>
2023-07-25 00:04:46 +0530

    Added translations boiler plate

---
+++ resources/js/Frontend/Data/translations.js
@@ -0,0 +1,11 @@
+/**
+ * All application translation will be defined here.
+ *
+ * @var const translations
+ */
+export const translations = {
+    locale: "en",
+    loading: false,
+    locales: ['en'],
+    fallbackLocale: "en"
+};

900a5f97a1977927ffc7aa9f6c2c3ba3a39cf728
Raman Verma <devramanverma@gmail.com>
2023-07-25 00:32:35 +0530

    Added util route

--- resources/js/Frontend/Utils/util.js
+++ resources/js/Frontend/Utils/util.js
@@ -2,3 +2,5 @@
 export { env } from '@/Utils/func/env';
 // Check wheather given string is empty.
 export { isEmpty } from '@/Utils/func/isEmpty';
+// Routes related stuffs
+export { route, routeName } from '@/Utils/func/route';

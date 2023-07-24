448d632388cbd648d0f5247912eaf4e80a5656ec
Raman Verma <devramanverma@gmail.com>
2023-07-25 00:04:23 +0530

    Added settings boiler plate

---
+++ resources/js/Frontend/Data/settings.js
@@ -0,0 +1,12 @@
+/**
+ * All application settings will be defined here.
+ * 
+ * @var const settings
+ */
+export const settings = {
+    app: {
+        name: 'Uy Scuti',
+        shorName: 'UyScuti',
+        summary: 'Search Engine using laravel as backend and react as frontend.'
+    }
+};

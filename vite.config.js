224215e59294bf35f4ce28302861bdebe3160ed0
Raman Verma <devramanverma@gmail.com>
2023-07-24 18:56:59 +0530

    Added plugin - [laravel-react-i18n/vite]

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import i18n from 'laravel-react-i18n/vite';
 
 export default defineConfig({
     plugins: [
@@ -12,7 +13,7 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        react()
+        react(),i18n()
     ],
     resolve: {
         alias: {

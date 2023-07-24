d350f0730a977661ecf8bfb38a916529094d5ead
Raman Verma <devramanverma@gmail.com>
2023-07-25 00:13:27 +0530

    Added translations reducer in store

--- resources/js/Frontend/Redux/stores/store.js
+++ resources/js/Frontend/Redux/stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import settingsReducer from '@/Redux/features/settings/settingsSlice';
+import translationsReducer from '@/Redux/features/translations/translationsSlice';
 
 export const store = configureStore({
   reducer: {
     settings: settingsReducer,
+    translations: translationsReducer
   },
 });

7fd69544d55fa67435c498ec5c8367ea9d881f2d
Raman Verma <devramanverma@gmail.com>
2023-07-28 16:49:48 +0530

    Added themes boiler plate

---
+++ resources/js/Frontend/Data/themes.js
@@ -0,0 +1,23 @@
+/**
+ * All application themes will be defined here.
+ *
+ * @var const themes
+ */
+export const themes = {
+    theme: "light",
+    mode: true,
+    themes: {
+        dark: {
+            mode: false,
+            schema: {
+                background: "#202124",
+            },
+        },
+        light: {
+            mode: true,
+            schema: {
+                background: "#ffffff",
+            },
+        },
+    },
+};

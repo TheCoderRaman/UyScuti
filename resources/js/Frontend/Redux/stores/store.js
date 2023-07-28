89f5d05a2b7fe9cbad2aee40351f0ecb732babaf
Raman Verma <devramanverma@gmail.com>
2023-07-28 16:53:44 +0530

    Added themes reducer in store

--- resources/js/Frontend/Redux/stores/store.js
+++ resources/js/Frontend/Redux/stores/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import themesReducer from '@/Redux/features/themes/themesSlice';
 import settingsReducer from '@/Redux/features/settings/settingsSlice';
 import translationsReducer from '@/Redux/features/translations/translationsSlice';
 
 export const store = configureStore({
   reducer: {
+    themes: themesReducer,
     settings: settingsReducer,
     translations: translationsReducer
   },

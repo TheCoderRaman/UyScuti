b34d215adc84f8ac7907a3720529d1c773b96977
Raman Verma <devramanverma@gmail.com>
2023-07-28 16:46:31 +0530

    Added tailwind configure file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./resources/**/*.vue",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

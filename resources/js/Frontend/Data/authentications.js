e66fd54751b62c43d05fa796cc6c0c8c108a465d
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:31:02 +0530

    Added default authentication data

---
+++ resources/js/Frontend/Data/authentications.js
@@ -0,0 +1,10 @@
+/**
+ * All authentications related stuff will be defined here.
+ * 
+ * @var const authentications
+ */
+export const authentications = {
+    user: null,
+    isLogged: false,
+    accessToken: null,
+};

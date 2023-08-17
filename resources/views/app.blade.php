c2685a60de6cb5d6d3feb03b14d4f9001921fb1d
Raman Verma <devramanverma@gmail.com>
2023-08-17 22:57:08 +0530

    Added APP_NAME through config helper

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>UyScuti</title>
+        <title>{{ config('APP_NAME','UyScuti') }}</title>
         
         @viteReactRefresh
         @vite([

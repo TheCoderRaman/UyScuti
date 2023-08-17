4c03bbaa10cc6bdf3146cdeb0a88db22cec303bc
Raman Verma <devramanverma@gmail.com>
2023-08-17 14:51:29 +0530

    Added  optional chaining operator

--- resources/js/Frontend/Utils/func/isEmpty.js
+++ resources/js/Frontend/Utils/func/isEmpty.js
@@ -1,9 +1,9 @@
 /**
- * Check wheather given value is empty.
- * 
+ * Check given value is empty.
+ *
  * This types will be treated as empty..
  * - [0, false, null, undefined]
- * 
+ *
  * @param any value
  * @return bool
  */
@@ -17,9 +17,9 @@ export function isEmpty(value){
         case 'number':
             return value === 0;
         case 'string':
-          return value.length <= 0 || value === '';
+          return value?.length <= 0 || value === '';
         case 'object': 
-          return value.length <= 0 || value === {} || value === [] || value === null;
+          return value?.length <= 0 || value === {} || value === [] || value === null;
     }
 
     return false;

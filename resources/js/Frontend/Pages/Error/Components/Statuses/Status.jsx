0ea61861c1c1b39422ca656e24c9bedd94217e85
Raman Verma <devramanverma@gmail.com>
2023-08-17 14:11:51 +0530

    Added status component

---
+++ resources/js/Frontend/Pages/Error/Components/Statuses/Status.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { useLaravelReactI18n } from 'laravel-react-i18n';
+
+function Status({
+    code
+}) {
+    const { pathname } = useLocation();
+    const { t } = useLaravelReactI18n();
+
+    return (
+        <div className='text-gray-500 dark:text-gray-400'>
+            {code !== 404 ? (
+                <>
+                    <div className='text-6xl pb-5'>
+                        <b>{t(`${code}`)}</b>
+                    </div>
+                    <p>
+                        <div className='text-4xl'>
+                            {t(`api.${code}`)}
+                        </div>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <p className='pb-5'>
+                        <b>{t(`frontend.${code}.`)} {' '}</b>
+                        {t('frontend.That\'s an error.')}
+                    </p>
+                    <p>
+                        {t('frontend.The requested URL')}
+                        <code>{pathname}{' '}</code>
+
+                        {t('frontend.was not found on this server.')} {' '}
+                        <br />
+                        <ins>{t('frontend.That\'s all we know.')}</ins>
+                    </p>
+                </>
+            )}
+        </div>
+    )
+}
+
+export default Status;

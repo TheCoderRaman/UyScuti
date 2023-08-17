84a0b049963e9ba3458fc748850828faaa9853f2
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:54:54 +0530

    Added custom hooks index

---
+++ resources/js/Frontend/Hooks/hooks.jsx
@@ -0,0 +1,8 @@
+// Use yup hook
+export { useYup } from '@/Hooks/useYup.jsx';
+// Use axiox hook
+export { useAxios } from '@/Hooks/useAxios.jsx';
+// Use paginate hook
+export { usePaginate } from '@/Hooks/usePaginate.jsx';
+// Use horizontal scroll hook
+export { useHorizontalScroll } from '@/Hooks/useHorizontalScroll.jsx';

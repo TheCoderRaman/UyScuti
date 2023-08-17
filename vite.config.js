e301d93549c3e9db60d450fb6e2553df025c8489
Raman Verma <devramanverma@gmail.com>
2023-08-17 22:54:55 +0530

    Added content and tailwindcss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import i18n from 'laravel-react-i18n/vite';
 
 export default defineConfig({
+    content: [
+        './resources/**/*.vue',
+        './resources/**/*.blade.php',
+        './resources/**/*.{js,ts,jsx,tsx}'
+    ],
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/js/index.jsx',
+                'resources/js/index.css',
             ],
             refresh: true,
         }),
-        react(),i18n()
+        tailwindcss(),react(),i18n()
     ],
     resolve: {
         alias: {

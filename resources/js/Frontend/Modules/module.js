6e04c7ca52a234323dd56feca3896767a6b55c88
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:56:34 +0530

    Updated imports

--- resources/js/Frontend/Modules/module.js
+++ resources/js/Frontend/Modules/module.js
@@ -1,2 +1,4 @@
+// Axios for interacting with external server
+export { axios } from '@/Modules/axios';
 // Local storage wrapper for interacting with local storage
-export { localStorage } from './LocalStorageSlim';
+export { localStorage } from '@/Modules/localStorageSlim';

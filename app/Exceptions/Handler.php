407ae507a07eada80264cbb7e8a62ef73617ebab
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:01:26 +0530

    Added response builder exception handler helper

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use MarcinOrlowski\ResponseBuilder\ExceptionHandlerHelper;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -25,6 +26,10 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (\Throwable $ex, $request) {
+            return ExceptionHandlerHelper::render($request, $ex);
         });
     }
 }

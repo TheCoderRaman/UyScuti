d9ebb1f86cbdc15097dfe0a1cf1776e201065baf
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:10:35 +0530

    Added api codes for response builder

---
+++ app/Utility/Api/ApiCodes.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\Utility\Api;
+
+class ApiCodes
+{
+    /**
+     * Http error codes as array
+     * 
+     * # HTTP STATUS CODES
+     */ 
+    public const HTTP_STATUS_CODES = [
+        # 1xx informational response
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+
+        # 2xx success
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+
+        # 3xx redirection
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+
+        # 4xx client errors
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        419 => 'Authentication Timeout',
+        420 => 'Enhance Your Calm',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        424 => 'Method Failure',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'No Response',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        451 => 'Unavailable For Legal Reasons',
+        494 => 'Request Header Too Large',
+        495 => 'Cert Error',
+        496 => 'No Cert',
+        497 => 'HTTP to HTTPS',
+        499 => 'Client Closed Request',
+
+        # 5xx server errors
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        598 => 'Network read timeout error',
+        599 => 'Network connect timeout error'
+    ];
+        
+    /**
+     * An informational response 
+     * indicates that the request 
+     * was received and understood.
+     * 
+     * # 1xx informational response
+     */
+    public const CONTINUE = 100;
+    public const SWITCHING_PROTOCOLS = 101;
+    public const PROCESSING = 102;
+    public const EARLY_HINTS = 103;
+
+    /**
+     * This class of status codes 
+     * indicates the action requested by 
+     * the client was received, understood, and accepted.
+     * 
+     * # 2xx success
+     */
+    public const OK = 200;
+    public const CREATED = 201;
+    public const ACCEPTED = 202;
+    public const NON_AUTHORITATIVE_INFORMATION = 203;
+    public const NO_CONTENT = 204;
+    public const RESET_CONTENT = 205;
+    public const PARTIAL_CONTENT = 206;
+    public const MULTI_STATUS = 207;
+    public const ALREADY_REPORTED = 208;
+    public const IM_USED = 226;
+
+    /**
+     * This class of status code indicates 
+     * the client must take additional action 
+     * to complete the request. Many of these 
+     * status codes are used in URL redirection.
+     * 
+     * # 3xx redirection
+     */  
+    public const MULTIPLE_CHOICES = 300;
+    public const MOVED_PERMANENTLY = 301;
+    public const FOUND = 302;
+    public const SEE_OTHER = 303;
+    public const NOT_MODIFIED = 304;  
+    public const USE_PROXY = 305;
+    public const SWITCH_PROXY = 306;
+    public const TEMPORARY_REDIRECT = 307;
+    public const PERMANENT_REDIRECT = 308;
+
+    /**
+     * This class of status code 
+     * is intended for situations 
+     * in which the error seems to 
+     * have been caused by the client.
+     * 
+     * # 4xx client errors
+     */     
+    public const BAD_REQUEST = 400;
+    public const UNAUTHORIZED = 401;
+    public const PAYMENT_REQUIRED = 402;
+    public const FORBIDDEN = 403;
+    public const NOT_FOUND = 404;
+    public const METHOD_NOT_ALLOWED = 405;
+    public const NOT_ACCEPTABLE = 406;
+    public const PROXY_AUTHENTICATION_REQUIRED = 407;
+    public const REQUEST_TIMEOUT = 408;
+    public const CONFLICT = 409;
+    public const GONE = 410;
+    public const LENGTH_REQUIRED = 411;
+    public const PRECONDITION_FAILED = 412;
+    public const PAYLOAD_TOO_LARGE = 413;
+    public const URI_TOO_LONG = 414;
+    public const UNSUPPORTED_MEDIA_TYPE = 415;
+    public const RANGE_NOT_SATISFIABLE = 416;
+    public const EXPECTATION_FAILED = 417;
+    public const IM_A_TEAPOT = 418;
+    public const MISDIRECTED_REQUEST = 421;
+    public const UNPROCESSABLE_ENTITY = 422;
+    public const LOCKED = 423;
+    public const FAILED_DEPENDENCY = 424;
+    public const TOO_EARLY = 425;
+    public const UPGRADE_REQUIRED = 426;
+    public const PRECONDITION_REQUIRED = 428;
+    public const TOO_MANY_REQUESTS = 429;
+    public const REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
+    public const UNAVAILABLE_FOR_LEGAL_REASONS = 451;
+
+    /**
+     * The server failed to fulfil a request.
+     * Response status codes beginning with the 
+     * digit 5 indicate cases in which the server 
+     * is aware that it has encountered an error or 
+     * is otherwise incapable of performing the request.
+     * 
+     * # 5xx server errors
+     */     
+    public const INTERNAL_SERVER_ERROR = 500;
+    public const NOT_IMPLEMENTED = 501;
+    public const BAD_GATEWAY = 502;
+    public const SERVICE_UNAVAILABLE = 503;
+    public const GATEWAY_TIMEOUT = 504;
+    public const HTTP_VERSION_NOT_SUPPORTED = 505;
+    public const VARIANT_ALSO_NEGOTIATES = 506;
+    public const INSUFFICIENT_STORAGE = 507;
+    public const LOOP_DETECTED = 508;
+    public const NOT_EXTENDED = 510;
+    public const NETWORK_AUTHENTICATION_REQUIRED = 511;
+}

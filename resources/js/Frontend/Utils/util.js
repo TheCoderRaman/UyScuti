885e089a62263c604be85deed82e17e8b4ee2988
Raman Verma <devramanverma@gmail.com>
2023-08-17 14:53:41 +0530

    Added geoData, frontendRoute, backendRoute, isObject, stringToBoolean

--- resources/js/Frontend/Utils/util.js
+++ resources/js/Frontend/Utils/util.js
@@ -1,8 +1,29 @@
+// Get geo data
+export {
+    getCity,
+    getRegion,
+    getCountry,
+    getTimeZone,
+    getGeoData
+} from '@/Utils/func/geoData';
+
+// Frontend routes
+export {
+    route as frontendRoute,
+    routeName as frontendRouteName,
+} from '@/Utils/func/frontendRoute';
+
+// Backend routes
+export {
+    route as backendRoute,
+    routeName as backendRouteName,
+} from '@/Utils/func/backendRoute';
+
 // Get environment variable from the env file.
 export { env } from '@/Utils/func/env';
-// Check wheather given string is empty.
+// Check given value is empty.
 export { isEmpty } from '@/Utils/func/isEmpty';
-// Routes related stuffs
-export { route, routeName } from '@/Utils/func/route';
+// Check given value is object.
+export { isObject } from '@/Utils/func/isObject';
 // Convert given string to boolean
-export { stringToBoolean } from '@/Utils/func/stringToBoolean'
+export { stringToBoolean } from '@/Utils/func/stringToBoolean';

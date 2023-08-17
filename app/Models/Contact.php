f294faa1716b1e8337ec4ab059be92e5d89c22e4
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:09:11 +0530

    Added contact for storing contact messages

---
+++ app/Models/Contact.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Contact extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'contact';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array $Dates
+     */
+    protected $Dates = [
+        'updated_at',
+        'created_at'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'message',
+        'updated_at',
+        'created_at'
+    ];
+}

f271ef3ad3ee3e397c4368df2e8b26c741ff18c8
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:21:26 +0530

    Added backend language lines

---
+++ lang/hi/backend.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backend Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during backend work for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    
+    'url not found' => 'यूआरएल नहीं मिला',
+    'ticket deleted' => 'टिकट हटा दिया गया',
+    'already subscribed' => 'पहले से ही सदस्यता ली',
+    'subscriber deleted' => 'ग्राहक को हटा दिया गया',
+    'url already queued' => 'यूआरएल पहले से ही कतारबद्ध',
+    'url already crawled' => 'यूआरएल पहले से ही क्रॉल किया गया',
+];

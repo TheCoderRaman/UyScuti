9bd01629593c32345a13bcb0099675a59bd480e5
Raman Verma <devramanverma@gmail.com>
2023-08-17 13:20:43 +0530

    Added backend language lines

---
+++ lang/en/backend.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backend Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during backend work for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    
+    'url not found' => 'url not found',
+    'ticket deleted' => 'ticket deleted',
+    'subscriber deleted' => 'subscriber deleted',
+    'url already queued' => 'url already queued',
+    'url already crawled' => 'url already crawled',
+    'already subscribed' => 'already subscribed',
+];

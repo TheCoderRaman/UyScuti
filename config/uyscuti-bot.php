d50c930c49ec51cad8fedeabb83fafe488b98a3f
Raman Verma <devramanverma@gmail.com>
2023-08-18 13:43:45 +0530

    Added concurrency config

--- config/uyscuti-bot.php
+++ config/uyscuti-bot.php
@@ -13,5 +13,18 @@ return [
      |
      | Supported: "profile", "allUrls", "subdomains", "internalUrls"
      */
-    'profile' => 'subdomains'
+    'profile' => 'subdomains',
+
+    /*
+     | --------------------------------------------------------------------------
+     | Concurrent Requests
+     | --------------------------------------------------------------------------
+     | You can tell the crawler how much urls to crawl concurrently.
+     |
+     | This option control the concurrent requests for crawling urls.
+     | you may specify any number here depending on your needs.
+     |
+     | Tip: improve the speed of the crawl
+     */
+    'concurrency' => 10,
 ];

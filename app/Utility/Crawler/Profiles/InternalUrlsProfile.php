4ea089c74588943923198c1e9de29d2ae60ef9e7
Raman Verma <devramanverma@gmail.com>
2023-08-18 13:37:01 +0530

    Removed already crawled console log

--- app/Utility/Crawler/Profiles/InternalUrlsProfile.php
+++ app/Utility/Crawler/Profiles/InternalUrlsProfile.php
@@ -4,7 +4,6 @@ namespace App\Utility\Crawler\Profiles;
 
 use Psr\Http\Message\UriInterface;
 use App\Utility\Urls\CrawledUrlAtlas;
-use App\Console\Commands\CrawlerCommand;
 use Spatie\Crawler\CrawlProfiles\CrawlInternalUrls;
 
 class InternalUrlsProfile extends CrawlInternalUrls
@@ -17,18 +16,8 @@ class InternalUrlsProfile extends CrawlInternalUrls
      */
     public function shouldCrawl(UriInterface $url): bool
     {
-        if(!app(CrawledUrlAtlas::class)->isExists($url)){
-            return true;
-        }
-
-        if(app()->runningInConsole()){
-            app(CrawlerCommand::class)->question(
-                sprintf("%s: Already Crawled Url [%s]",
-                    substr(strrchr(get_class($this), '\\'), 1), $url
-                )
-            );
-        }
-
-        return false;
+        return !(app(
+            CrawledUrlAtlas::class)->isExists($url)
+        );
     }
 }

1c928b122be7c24c33b758a1fed624239f3fb6e3
Raman Verma <devramanverma@gmail.com>
2023-08-18 13:13:56 +0530

    Added crawl profile config

--- config/uyscuti-bot.php
+++ config/uyscuti-bot.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    //
+
+    /*
+     | --------------------------------------------------------------------------
+     | Crawl Profile
+     | --------------------------------------------------------------------------
+     | You can tell the crawler not to visit certain urls by using the profile.
+     |
+     | This option control which profile to use during crawling urls.
+     | you may specify any of the wonderful profiles provided here.
+     |
+     | Supported: "profile", "allUrls", "subdomains", "internalUrls"
+     */
+    'profile' => 'subdomainsProfile'
 ];

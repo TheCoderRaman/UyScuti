e62d43de5946f32ebdae369541d8b6f0200492d2
Raman Verma <devramanverma@gmail.com>
2023-08-18 13:12:07 +0530

    Added crawler subdomains urls profile

---
+++ app/Utility/Crawler/Profiles/SubdomainsProfile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Utility\Crawler\Profiles;
+
+use Psr\Http\Message\UriInterface;
+use App\Utility\Urls\CrawledUrlAtlas;
+use App\Console\Commands\CrawlerCommand;
+use Spatie\Crawler\CrawlProfiles\CrawlSubdomains;
+
+class SubdomainsProfile extends CrawlSubdomains
+{
+    /**
+     * Determine if the given url should be crawled.
+     *
+     * @param UriInterface $url
+     * @return bool
+     */
+    public function shouldCrawl(UriInterface $url): bool
+    {
+        if(!app(CrawledUrlAtlas::class)->isExists($url)){
+            return true;
+        }
+
+        if(app()->runningInConsole()){
+            app(CrawlerCommand::class)->question(
+                sprintf("%s: Already Crawled Url [%s]",
+                    substr(strrchr(get_class($this), '\\'), 1), $url
+                )
+            );
+        }
+
+        return false;
+    }
+}

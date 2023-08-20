08542cae6d6558d3bddb628a533daa342b8654fe
Animax Dev <arkanimetube@gmail.com>
2023-08-20 22:28:06 +0530

    Fixed minimal responsiveness

--- resources/js/Frontend/Components/Layouts/MinimalLayout.jsx
+++ resources/js/Frontend/Components/Layouts/MinimalLayout.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function MinimalLayout(props) {
     return (
-        <div className="flex flex-col min-h-screen h-[100%] justify-between  bg-[#fff] dark:bg-[#202124] text-[#202124] dark:text[#bdc1c6] min-w-[650px]">
+        <div 
+            style={{
+                width: '100vw',
+                minWidth: '350px',
+            }}
+            className="flex flex-col min-h-screen h-[100%] justify-between bg-[#fff] dark:bg-[#202124] text-[#202124] dark:text[#bdc1c6]"
+        >
             {/* Main Section */}
             <main className="main-layout">
                 {props.children}
